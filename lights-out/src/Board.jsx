b03e09e88244537fc71078192a7aaa733dea9fbf
Rigved <rigvedrs@gmail.com>
2021-03-01 11:54:40 +0530

    Crack a Laugh, First Commit

---
+++ lights-out/src/Board.jsx
@@ -0,0 +1,86 @@
+import React, {useState} from "react";
+import Cell from "./Cell";
+import './Board.css';
+
+/** Game board of Lights out.
+ *
+ * Properties:
+ *
+ * - nrows: number of rows of board
+ * - ncols: number of cols of board
+ * - chanceLightStartsOn: float, chance any cell is lit at start of game
+ *
+ * State:
+ *
+ * - hasWon: boolean, true when board is all off
+ * - board: array-of-arrays of true/false
+ *
+ *    For this board:
+ *       .  .  .
+ *       O  O  .     (where . is off, and O is on)
+ *       .  .  .
+ *
+ *    This would be: [[f, f, f], [t, t, f], [f, f, f]]
+ *
+ *  This should render an HTML table of individual <Cell /> components.
+ *
+ *  This doesn't handle any clicks --- clicks are on individual cells
+ *
+ **/
+
+function Board(props) {
+
+  // TODO: set initial state
+  
+
+  /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
+
+  function createBoard() {
+    let board = [];
+    // TODO: create array-of-arrays of true/false values
+    return board
+  }
+
+  /** handle changing a cell: update board & determine if winner */
+
+  function flipCellsAround(coord) {
+    let {ncols, nrows} = this.props;
+    let board = this.state.board;
+    let [y, x] = coord.split("-").map(Number);
+
+
+    function flipCell(y, x) {
+      // if this coord is actually on board, flip it
+
+      if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
+        board[y][x] = !board[y][x];
+      }
+    }
+
+    // TODO: flip this cell and the cells around it
+
+    // win when every cell is turned off
+    // TODO: determine is the game has been won
+
+    setState(board);
+    setState(hasWon);
+  }
+
+
+  /** Render game board or winning message. */
+
+  return (
+
+    // if the game is won, just show a winning msg along with the leaderboard
+
+    // TODO
+
+    // make table board
+    // render leaderboard when won or lost
+
+    // TODO
+  );
+}
+
+
+export default Board;
